7b082925fe6db5a767f943b1b88d68bb9293b505
Kirsty Selesho (Britehouse) <Kirsty.Selesho@britehouse.co.za>
2019-11-05 14:38:21 +0200

    work

---
+++ Ft_BubbleSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ft_BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {5, 0, 3, 1, 10};
+            int t;
+            Console.WriteLine("Original array :");
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            for(int p = 0; p <= numbers.Length - 2; p++)
+            {
+                for(int i = 0; i <= numbers.Length - 2; i++)
+                {
+                    if(numbers[i] > numbers[i + 1])
+                    {
+                        t = numbers[i + 1];
+                        numbers[i + 1] = numbers[i];
+                        numbers[i] = t;
+                    }
+                }
+            }
+            Console.WriteLine("\n" + "Sorted Array :");
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.ReadLine();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
